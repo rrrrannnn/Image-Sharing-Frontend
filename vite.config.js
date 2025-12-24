88ec2e4bed3d642ed81fc2f2d43f9981f8d6c839
muziran <gys54818901@gmail.com>
2025-12-24 23:35:50 +0800

    更新了后端api地址

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8123',
+        // target: 'http://localhost:8123',
+        target: 'https://image-sharing-backend-mzr-eradcrcdedctbqbr.uksouth-01.azurewebsites.net',
         changeOrigin: true
       }
     }
